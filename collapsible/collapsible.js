21f149f60a3311c0d1b6bbe5c654dd9e1e3ab02a
Mai X <mxio@users.noreply.github.com>
2016-10-22 13:02:35 -0600

    Create collapsible.js

---
+++ collapsible/collapsible.js
@@ -0,0 +1,12 @@
+var list = document.getElementById('menu');
+
+// the open class is not used to add styles, just keep track of whether the item is open or closed
+list.addEventListener('click', function(event) {
+  if (event.target.nodeName === 'INPUT') {
+    if (event.target.classList.contains('open')) {
+      event.target.checked = false;
+    }
+    // if it was open when we clicked it, remove the open class; otherwise add the open class
+    event.target.classList.toggle('open');
+  }
+}, false);
