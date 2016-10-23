9c0f14aededdc5f9a1fb03af204fc2321a8e60b9
Melissa <meowwwls@users.noreply.github.com>
2016-10-22 19:39:06 -0500

    fix click target
    When we were targeting the input, sometimes the click would not register. Now we're targeting the label and it seems to work every time.

--- collapsible/collapsible.js
+++ collapsible/collapsible.js
@@ -2,11 +2,16 @@ var list = document.getElementById('menu');
 
 // the open class is not used to add styles, just keep track of whether the item is open or closed
 list.addEventListener('click', function(event) {
-  if (event.target.nodeName === 'INPUT') {
-    if (event.target.classList.contains('open')) {
-      event.target.checked = false;
+  var input;
+  if (event.target.nodeName === 'LABEL') {
+    input = event.target;
+    // if item is already open, close it
+    if (input.classList.contains('open')) {
+      input.classList.remove('open');
+      input.checked = false;
+    } else  {
+      // otherwise this is the opening click, so add tracking 'open' class
+      input.classList.add('open');
     }
-    // if it was open when we clicked it, remove the open class; otherwise add the open class
-    event.target.classList.toggle('open');
-  }
+  } 
 }, false);
