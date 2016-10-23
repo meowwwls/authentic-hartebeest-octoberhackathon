2865e11165e73235f550640f00c6fc44d209539b
Melissa <meowwwls@users.noreply.github.com>
2016-10-22 20:51:23 -0500

    Update collapsible.js

--- collapsible/collapsible.js
+++ collapsible/collapsible.js
@@ -3,13 +3,14 @@ var list = document.getElementById('menu');
 var lastClicked = '';
 
 list.addEventListener('click', function(event) {
+  var tClass = event.target.classList; // storing for easy access since it's referenced so much
   if (event.target.nodeName === 'INPUT') {
     // if we clicked on the same item twice in a row, it's open; uncheck it
-    if (event.target.classList.contains('open') && event.target === lastClicked) {
+    if (tClass.contains('open') && event.target === lastClicked) {
       event.target.checked = false;
-      event.target.classList.remove('open');
-  } else {
-    event.target.classList.add('open');
+      tClass.remove('open');
+  } else if (!tClass.contains('open')) { // add class of open if it's not already there
+    tClass.add('open');
   }
   // current element = lastClicked so we can check it against the next element clicked
   lastClicked = event.target;
